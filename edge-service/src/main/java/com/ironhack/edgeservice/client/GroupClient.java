cffd45e9e960b99795321004fd1917cea82a8702
Iván Trujillo <ivtrujillo12@gmail.com>
2021-03-09 13:29:47 +0000

    Finished save new group on edge service

--- edge-service/src/main/java/com/ironhack/edgeservice/client/GroupClient.java
+++ edge-service/src/main/java/com/ironhack/edgeservice/client/GroupClient.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -22,4 +23,7 @@ public interface GroupClient {
 
     @GetMapping("/members-group/{id}")
     List<Long> getMembersByGroupId(@PathVariable("id") Long id);
+
+    @PostMapping("/new-group")
+    GroupDTO saveNewGroup(@RequestBody GroupDTO groupDTO);
 }
